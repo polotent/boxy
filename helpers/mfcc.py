e67810e02c35de2cfb74ce57ba007c4419216d31
polotent <alexmirosh37@gmail.com>
2021-02-07 12:57:07 +0300

    mel scale transfrom functions

--- helpers/mfcc.py
+++ helpers/mfcc.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+def freq_to_mel(freq):
+    return 1127 * np.log(1 + freq / 700)
+
+def mel_to_freq(mel):
+    return 700 * (np.exp(mel / 1127) - 1)
 
 def mfcc(frames, n_mel_coeffs): 
     pass
