f7d85c93cd7d73945b1ff0f7ff1f744c8b79ad84
polotent <alexmirosh37@gmail.com>
2021-03-04 16:49:22 +0300

    linter fixes + linux compatibility

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import os
 from scipy.io import wavfile
 from matplotlib.backends.backend_pdf import PdfPages
-from python_speech_features import mfcc
+# from python_speech_features import mfcc
 import helpers as hp
 import numpy as np
 
@@ -23,6 +23,7 @@ UNIFIED_LENGTH = 400
 if HOP_SIZE > FRAME_SIZE:
     raise ValueError('HOP_SIZE constant must be less than or equals FRAME_SIZE')
 
+
 def process_single_audio(audio, sample_rate, filename):
     original = audio
     hp.print_audio_info(audio, sample_rate, filename)
@@ -31,21 +32,23 @@ def process_single_audio(audio, sample_rate, filename):
     audio = hp.remove_DC_offset(audio)
     frames = hp.split_into_frames(audio, sample_rate, FRAME_SIZE, HOP_SIZE)
     voice_frames = hp.get_voice_frames(frames, FRAME_SIZE, HOP_SIZE, INIT_LENGTH, SEARCH_LENGTH)
-    coeffs = hp.mfcc(voice_frames, sample_rate, n_coeffs=13, n_filters=26, low_freq=0, high_freq=(sample_rate // 2))    
+    coeffs = hp.mfcc(voice_frames, sample_rate, n_coeffs=13, n_filters=26, low_freq=0, high_freq=(sample_rate // 2))
     extracted = hp.join_frames(voice_frames, sample_rate, HOP_SIZE)
 
-    ## python_speech_features version
-    # coeffs = mfcc(extracted, sample_rate, winlen=FRAME_SIZE/1000, winstep=HOP_SIZE/1000) 
-    ## ------------------------------
-    
+    # python_speech_features version
+    # coeffs = mfcc(extracted, sample_rate, winlen=FRAME_SIZE/1000, winstep=HOP_SIZE/1000)
+    # ------------------------------
+
     return original, extracted, coeffs
 
-def process_folder(folder_path, commands_path, save_exctracted=False, save_path=None, generate_report=False, report_path=None):
+
+def process_folder(folder_path, commands_path, save_exctracted=False, save_path=None,
+                   generate_report=False, report_path=None):
     if not os.path.isdir(save_path):
         os.mkdir(save_path)
     if generate_report:
         pdf_report = PdfPages(report_path)
-    
+
     commands = hp.get_commands_dict(commands_path)
 
     files = list()
@@ -65,11 +68,11 @@ def process_folder(folder_path, commands_path, save_exctracted=False, save_path=
         splited = files[i][2].split('-')
         if len(splited) < 2:
             raise ValueError('Invalid filename for extracting command from it. Filename must'
-                                'contain command after record number. E.g. \'0-command-info.wav\'')
+                             'contain command after record number. E.g. \'0-command-info.wav\'')
             exit()
         else:
             command = splited[1]
-        
+
         if command not in commands:
             raise Warning(f'No command \'{command}\' found in commands dict. Passing over')
         else:
@@ -80,7 +83,7 @@ def process_folder(folder_path, commands_path, save_exctracted=False, save_path=
         if save_exctracted:
             basename = os.path.basename(files[i][0])
             path = os.path.join(save_path, files[i][1], f'{os.path.splitext(basename)[0]}_extracted.wav')
-            
+
             if not os.path.isdir(os.path.dirname(path)):
                 os.mkdir(os.path.dirname(path))
             wavfile.write(path, sample_rate, audio)
@@ -96,9 +99,11 @@ def process_folder(folder_path, commands_path, save_exctracted=False, save_path=
 
 
 if __name__ == '__main__':
-    process_folder(folder_path='D:\\MyFiles\\projects\\boxy\\recorded_audio',
-                   commands_path='D:\\MyFiles\\projects\\boxy\\commands.csv',
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+
+    process_folder(folder_path=os.path.join(curr_dir, 'recorded_audio'),
+                   commands_path=os.path.join(curr_dir, 'commands.csv'),
                    save_exctracted=False,
-                   save_path='D:\\MyFiles\\projects\\boxy\\data',
+                   save_path=os.path.join(curr_dir, 'data'),
                    generate_report=True,
-                   report_path='D:\\MyFiles\\projects\\boxy\\data\\report.pdf')    
+                   report_path=os.path.join(curr_dir, 'data/report.pdf'))
