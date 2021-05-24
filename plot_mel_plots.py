e30f07b7c44646d8648a9215560464cc32b791e3
polotent <alexmirosh37@gmail.com>
2021-05-24 22:45:41 +0300

    moved plot_mel_plots to helpers module

--- plot_mel_plots.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import helpers as hp
-
-
-if __name__ == '__main__':
-    x = np.linspace(0, 22050, 22050)
-    hp.plot_mel_hz(x, hp.hz_to_mel(x))
-    hp.plot_mel_filters(hp.get_filters(13,1024, 44100, 0, 22050), 0, 22050)
